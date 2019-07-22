f580b5f9d6e5a02a438990994574b67559a06ca9
Rayos <rayos.dev@gmail.com>
2019-07-22 07:54:52 +0200

    Updates

--- app.js
+++ app.js
@@ -1,6 +1,41 @@
-const App = document.querySelector('#app');
-ReactDOM.render(
-    
-    e(<h1>Hola</h1>)
-    
-    , App);
+
+class App extends React.Component{
+    render() {
+        return(
+            <div className="main-container">
+                <h1>Hey man</h1>
+                {/* <Construction /> */}
+            </div>
+        )
+    }
+}
+
+
+class Header extends React.Component{
+    render() {
+        return (
+            <div className="header">
+                
+            </div>
+        )
+    }
+}
+
+
+class Construction extends React.Component{
+    render() {
+        return (
+            <div className="construction__container">
+                <h1 className="construction__header-text">Under Construction</h1>
+                <p className="construction__paragraph">The site will be up agian soon</p>
+                <img className="construction__image" src="img/construction.svg" alt="" />
+            </div>
+        )
+    }
+}
+
+
+
+
+
+ReactDOM.render(<App />, document.getElementById("app"))
